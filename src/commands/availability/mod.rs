7bc0737ec9525ee7e211c6a65733518584b1a20b
Randy Bailey <rrbailey89@gmail.com>
2025-03-15 15:23:09 -0700

    Update mod.rs

--- src/commands/availability/mod.rs
+++ src/commands/availability/mod.rs
@@ -2,8 +2,6 @@ mod unavailable;
 mod cancel_unavailable;
 mod list_unavailable;
 mod set_unavailability_channel;
-mod iscaliforniaonfire;
-mod whereiscaliforniaonfire;
 
 use crate::error::Error;
 use crate::Data;
@@ -30,17 +28,5 @@ pub fn listunavailable() -> poise::Command<Data, Error> {
 pub fn setunavailabilitychannel() -> poise::Command<Data, Error> {
     let mut cmd = set_unavailability_channel::setunavailabilitychannel();
     cmd.category = Some("Availability".to_string());
-    cmd
-}
-
-pub fn iscaliforniaonfire() -> poise::Command<Data, Error> {
-    let mut cmd = iscaliforniaonfire::iscaliforniaonfire();
-    cmd.category = Some("Utility".to_string());  // Changed from "Availability" to "Utility"
-    cmd
-}
-
-pub fn whereiscaliforniaonfire() -> poise::Command<Data, Error> {
-    let mut cmd = whereiscaliforniaonfire::whereiscaliforniaonfire();
-    cmd.category = Some("Utility".to_string());  // Changed from "Availability" to "Utility"
     cmd
 }
